d3eb1d4db63aa38a4d80133ff604a25881f15d94
shwzr <showzur@gmail.com>
2024-02-10 13:36:04 +0100

    second commit

--- FuseVideo_Ad_Blocker/FuseVideo_Ad_Blocker.js
+++ FuseVideo_Ad_Blocker/FuseVideo_Ad_Blocker.js
@@ -1,26 +1,26 @@
 // ==UserScript==
 // @name         FuseVideo Ad Blocker
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @license      MIT
 // @description  Blocks intrusive ads on FuseVideo streaming links.
 // @author       Showzur
-// @match        *://*/*
+// @match        *://*.animecat.net/*
+// @match        *://*.neko-sama.fr/*
+// @match        *://*.fusevideo.io/*
 // @grant        none
-// @downloadURL https://update.greasyfork.org/scripts/485272/FuseVideo%20Ad%20Blocker.user.js
-// @updateURL https://update.greasyfork.org/scripts/485272/FuseVideo%20Ad%20Blocker.meta.js
 // ==/UserScript==
-
+ 
 (function() {
     'use strict';
-
+ 
     setTimeout(function() {
         var elements = document.querySelectorAll('[rel="noopener noreferrer nofollower"]');
-
+ 
         elements.forEach(function(element) {
             element.parentNode.removeChild(element);
         });
-
+ 
         console.log(elements.length + " éléments supprimés.");
     }, 1000);
 })();
